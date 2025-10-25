e06bf046d972e3b81018136e72a3d49fc5af0622
Max <maxpaleev06@gmail.com>
2025-10-25 21:28:58 +0300

    Add new task tab, clear all after add event

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import sys
 import io
 
 from PyQt6 import uic
+from PyQt6.QtCore import QTime
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
 
 
 class SimplePlanner(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('design.ui', self)
+        uic.loadUi('design_test.ui', self)
         self.setWindowTitle('Минипланировщик')
         self.events = []
         self.addEventBtn.clicked.connect(self.add)
 
     def add(self):
-        if self.lineEdit.text():
+        if self.eventName.text():
             event_date = datetime(self.calendarWidget.selectedDate().year(), self.calendarWidget.selectedDate().month(),
                          self.calendarWidget.selectedDate().day(), self.timeStart.time().hour(),
                          self.timeStart.time().minute())
@@ -25,11 +26,15 @@ class SimplePlanner(QMainWindow):
             if event_end < event_start:
                 QMessageBox(self).critical(self, "Ошибка", "Время окончания события не может быть раньше времени начала")
             else:
-                my_event = [event_date, event_end, self.lineEdit.text()]
+                my_event = [event_date, event_end, self.eventName.text()]
                 self.events.append(my_event)
+                self.eventName.clear()
 
                 self.events = sorted(self.events, key=lambda x: x[0])
                 self.eventList.clear()
+                self.timeStart.setTime(QTime(0, 0))
+                self.timeEnd.setTime(QTime(0, 0))
+                self.calendarWidget.setSelectedDate(date.today())
 
                 for i in self.events:
                     self.eventList.addItem(i[0].strftime("%d.%m.%Y %H:%M") + " - " + i[1].strftime("%H:%M") + " : " + i[2])
