5db934390912dd309e50b2f9d7c3c6869c18cb66
Max <maxpaleev06@gmail.com>
2025-10-25 15:49:56 +0300

    Start

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import datetime
+import sys
+import io
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
-# Press the green button in the gutter to run the script.
+
+class DiaryEvent():
+    def __init__(self, datetime, title):
+        self.datetime = datetime
+        self.title = title
+
+    def to_str(self):
+        return "{} - {}".format(self.datetime, self.title)
+
+    def __str__(self):
+        return self.to_str()
+
+
+class SimplePlanner(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('design.ui', self)
+        self.setWindowTitle('Минипланировщик')
+        self.events = []
+        self.addEventBtn.clicked.connect(self.add)
+
+    def add(self):
+        if self.lineEdit.text():
+            t = datetime.datetime(self.calendarWidget.selectedDate().year(), self.calendarWidget.selectedDate().month(),
+                                  self.calendarWidget.selectedDate().day(), self.timeEdit.time().hour(),
+                                  self.timeEdit.time().minute())
+
+            my_event = DiaryEvent(t, self.lineEdit.text())
+            self.events.append(my_event)
+
+            self.events = sorted(self.events, key=lambda x: x.datetime)
+            self.eventList.clear()
+
+            self.eventList.addItems([i.to_str() for i in self.events])
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    form = SimplePlanner()
+    form.show()
+    sys.exit(app.exec())
