a64084d69f47f9318e58ccc88045e568313cac6e
Max <maxpaleev06@gmail.com>
2025-10-26 16:43:00 +0300

    Объединение событий по дате

--- main.py
+++ main.py
@@ -3,41 +3,54 @@ import sys
 import io
 
 from PyQt6 import uic
-from PyQt6.QtCore import QTime
-from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QTreeWidget, QTreeWidgetItem
 
 
 class SimplePlanner(QMainWindow):
+    # создание окна
     def __init__(self):
         super().__init__()
         uic.loadUi('design_test.ui', self)
         self.setWindowTitle('Минипланировщик')
         self.events = []
         self.addEventBtn.clicked.connect(self.add)
+        self.data = {}
+        self.eventList.setColumnCount(2)
+        self.eventList.setHeaderLabels(["Событие", "Время"])
 
+    # добавление события
     def add(self):
         if self.eventName.text():
-            event_date = datetime(self.calendarWidget.selectedDate().year(), self.calendarWidget.selectedDate().month(),
-                         self.calendarWidget.selectedDate().day(), self.timeStart.time().hour(),
-                         self.timeStart.time().minute())
+            # разделение времени
+            event_day = datetime(self.calendarWidget.selectedDate().year(), self.calendarWidget.selectedDate().month(),
+                                 self.calendarWidget.selectedDate().day())
             event_start = time(hour=self.timeStart.time().hour(), minute=self.timeStart.time().minute())
             event_end = time(hour=self.timeEnd.time().hour(), minute=self.timeEnd.time().minute())
 
+            # проверка на корректность времени
             if event_end < event_start:
-                QMessageBox(self).critical(self, "Ошибка", "Время окончания события не может быть раньше времени начала")
+                QMessageBox(self).critical(self, "Ошибка",
+                                           "Время окончания события не может быть раньше времени начала")
             else:
-                my_event = [event_date, event_end, self.eventName.text()]
-                self.events.append(my_event)
-                self.eventName.clear()
+                if event_day in self.data:
+                    self.data[event_day].append([self.eventName.text(), event_start, event_end])
+                else:
+                    self.data[event_day] = [[self.eventName.text(), event_start, event_end]]
 
-                self.events = sorted(self.events, key=lambda x: x[0])
                 self.eventList.clear()
-                self.timeStart.setTime(QTime(0, 0))
-                self.timeEnd.setTime(QTime(0, 0))
-                self.calendarWidget.setSelectedDate(date.today())
 
-                for i in self.events:
-                    self.eventList.addItem(i[0].strftime("%d.%m.%Y %H:%M") + " - " + i[1].strftime("%H:%M") + " : " + i[2])
+                # сортировка и добавление в виджет
+                items = []
+                for key, values in sorted(self.data.items(), key=lambda x: x[0]):
+                    item = QTreeWidgetItem([key.strftime("%d.%m.%Y")])
+                    for value in sorted(values, key=lambda x: x[1]):
+                        name = value[0]
+                        time_str = value[1].strftime("%H:%M") + " - " + value[2].strftime("%H:%M")
+                        child = QTreeWidgetItem([name, time_str])
+                        item.addChild(child)
+                    items.append(item)
+
+                self.eventList.insertTopLevelItems(0, items)
 
 
 if __name__ == '__main__':
